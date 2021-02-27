393f66659bdc0df673aa1b6e591bfe89e44be974
Paulo Afonso <paulolimagatti@gmail.com>
2021-02-27 10:43:50 -0300

    Refactoring docker configs (and the sh file) in order to use docker-compose and environment variables

--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -1,4 +1,2 @@
-docker stop -t0 ic-container &&
-docker container rm ic-container &&
-docker build -t ic-image . &&
-docker run -d --restart=unless-stopped --mount type=bind,source="$(pwd)",target=/var/www/inventory-control -p 8081:80 --name ic-container ic-image
+docker-compose down -t0 &&
+docker-compose up --build -d
